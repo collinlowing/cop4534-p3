4e4ddaaa730cf1ce0e14c9ea4f1bc90ab02b3bfc
Collin Lowing <collin.lowing@protonmail.com>
2022-10-17 14:17:34 -0500

    test file parser pass

--- Google_Tests/testFileParser.cpp
+++ Google_Tests/testFileParser.cpp
@@ -1,3 +1,22 @@
 //
 // Created by cel on 10/13/22.
 //
+
+#include "gtest/gtest.h"
+#include "../FileParser.hpp"
+
+TEST(FileParserTests, getDistancesFromFile) {
+    std::vector<double> distances = FileParser::getDistancesFromFile("distances.txt");
+
+    EXPECT_EQ(distances.at(0), 25.410000);
+    EXPECT_EQ(distances.at(1), 109.670000);
+    EXPECT_EQ(distances.at(2), 88.340000);
+    EXPECT_EQ(distances.at(3), 90.000000);
+    EXPECT_EQ(distances.at(4), 116.690000);
+    EXPECT_EQ(distances.at(5), 82.240000);
+    EXPECT_EQ(distances.at(6), 39.780000);
+    EXPECT_EQ(distances.at(7), 118.580000);
+    EXPECT_EQ(distances.at(8), 94.620000);
+    EXPECT_EQ(distances.at(9), 69.640000);
+
+}
